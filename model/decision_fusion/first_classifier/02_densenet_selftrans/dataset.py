b266c152bf87e8817602de26e5363d8dec56077a
Ian <824999404@qq.com>
2021-01-01 22:16:18 +0800

    Add moco

--- model/decision_fusion/first_classifier/02_densenet_selftrans/dataset.py
+++ model/decision_fusion/first_classifier/02_densenet_selftrans/dataset.py
@@ -22,3 +22,20 @@ class listDataset(Dataset):
         img_path = self.lines[index]
         img, label = load_data(img_path)
         return img, label
+
+
+class mocoDataset(Dataset):
+    def __init__(self, root, train=False , aug_plus=True):
+        random.shuffle(root)
+        self.nSamples = len(root)
+        self.lines = root
+        self.aug_plus = aug_plus
+
+    def __len__(self):
+        return self.nSamples
+
+    def __getitem__(self, index):
+        assert index <= len(self), 'index range error'         
+        img_path = self.lines[index]
+        imgs, label = load_data_moco(img_path,aug_plus=self.aug_plus)
+        return imgs, label
--- model/decision_fusion/first_classifier/02_densenet_selftrans/image.py
+++ model/decision_fusion/first_classifier/02_densenet_selftrans/image.py
@@ -36,3 +36,70 @@ def load_data(img_path, train = True):
     ones = torch.sparse.torch.eye(3)
     label = ones.index_select(0, torch.tensor([label_index]))
     return image, label
+
+
+def load_data_moco(img_path, train = True , aug_plus=True):
+    
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+    if aug_plus:
+        # MoCo v2's aug: similar to SimCLR https://arxiv.org/abs/2002.05709
+        augmentation = [
+            transforms.Resize(256),
+            transforms.RandomResizedCrop(224, scale=(0.2, 1.)),
+            transforms.RandomApply([
+                transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)  # not strengthened
+            ], p=0.8),
+            transforms.RandomGrayscale(p=0.2),
+            transforms.RandomApply([GaussianBlur([.1, 2.])], p=0.5),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize
+        ]
+    else:
+        # MoCo v1's aug: the same as InstDisc https://arxiv.org/abs/1805.01978
+        augmentation = [
+            transforms.Resize(256),
+            transforms.RandomResizedCrop(224, scale=(0.2, 1.)),
+            transforms.RandomGrayscale(p=0.2),
+            transforms.ColorJitter(0.4, 0.4, 0.4, 0.4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize
+        ]
+
+    transform = TwoCropsTransform(transforms.Compose(augmentation))
+
+    image = Image.open(img_path).convert('RGB')
+    img_q,img_k = transform(image)
+    images = torch.tensor([np.array(img_q),np.array(img_k)])
+    label_index = int(img_path.split('.')[0][-1])
+    assert label_index in [0, 1, 2]
+    ones = torch.sparse.torch.eye(3)
+    label = ones.index_select(0, torch.tensor([label_index]))
+    return images, label
+
+
+
+class TwoCropsTransform:
+    """Take two random crops of one image as the query and key."""
+
+    def __init__(self, base_transform):
+        self.base_transform = base_transform
+
+    def __call__(self, x):
+        q = self.base_transform(x)
+        k = self.base_transform(x)
+        return [q, k]
+
+
+class GaussianBlur(object):
+    """Gaussian blur augmentation in SimCLR https://arxiv.org/abs/2002.05709"""
+
+    def __init__(self, sigma=[.1, 2.]):
+        self.sigma = sigma
+
+    def __call__(self, x):
+        sigma = random.uniform(self.sigma[0], self.sigma[1])
+        x = x.filter(ImageFilter.GaussianBlur(radius=sigma))
+        return x
